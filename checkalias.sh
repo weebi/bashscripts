47e669cf011547819fad80d36f8b09cdd54d82bc
weebo <weebi@national.shitposting.agency>
2021-05-21 01:58:58 +0300

    Added checkalias.sh

---
+++ checkalias.sh
@@ -0,0 +1,6 @@
+# if alias points to script, show contents
+function check_alias(){
+    [[ ! -z "${1}" && $(alias "${1}" | sed -e 's|['\''"]$||g' | grep -o ...$) == ".sh" ]] && cat $(alias "${1}" | sed -e 's|\w.*=['\''"]||g' -e 's|['\''"]$||g' -e 's| |\\ |g')
+}
+
+check_alias "${1}"
