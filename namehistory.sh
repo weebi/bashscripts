b22faaff7c57b5ddb619a356f72746266e148e12
weebi <weebi@national.shitposting.agency>
2021-05-22 01:04:20 +0300

    Added namehistory.sh

---
+++ namehistory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Gets name history of the selected user if it exists
+namehistory() {
+	[[ -n "${1}" ]] && echo "Usage: ${0} <username>" && exit 1
+
+	# Due to how Mojang's API works, we need to turn the username into an ID first
+	local userId=$(curl -s "https://api.mojang.com/users/profiles/minecraft/${1}" | jq -r .id)
+
+	[[ -n "${userId}" ]] && curl -s "https://api.mojang.com/user/profiles/$userID/names" | jq -r ".[] | .name" || echo "Error in fetching data, either the account doesn't exist or you're disconnected from the internet" > /dev/stderr
+}
+
+namehistory "${1}"
